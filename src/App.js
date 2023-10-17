e71a7b4f8b210dd643386f5eaf4e7c11889ead58
8mitsuboy <hachimitsu8897@icloud.com>
2023-10-17 10:28:55 +0900

    計測機能の追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+
+export const App = () => {
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
+  const [timeDifference, setTimeDifference] = useState(null);
+  const [textFlag, setTextFlag] = useState(true);
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        if (startTime === null) {
+          // 最初のEnterキーが押されたとき
+          setStartTime(Date.now());
+          setTimeDifference(null);
+          setTextFlag(!textFlag);
+        } else if (endTime === null) {
+          // 2回目のEnterキーが押されたとき
+          setEndTime(Date.now());
+          setTextFlag(!textFlag);
+        }
+      }
+    };
+
+    document.addEventListener('keypress', handleKeyPress);
+
+    return () => {
+      // コンポーネントがアンマウントされるときにイベントリスナーを削除
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, [startTime, endTime]);
+
+  useEffect(() => {
+    if (startTime !== null && endTime !== null) {
+      const difference = endTime - startTime;
+      setTimeDifference(difference);
+      setStartTime(null); // 計測終了後、再度計測できるように初期化
+      setEndTime(null);
+    }
+  }, [startTime, endTime]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        {textFlag
+          ? 'エンターキーを押して計測開始'
+          : 'もう一度エンターキーを押して計測終了'}
+      </p>
+      {timeDifference !== null && (
+        <p>Enterキーを2回押すまでの時間: {timeDifference} ミリ秒</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
