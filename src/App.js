899055a450f236a93968123abec4c1ade3e8bbcb
8mitsuboy <hachimitsu8897@icloud.com>
2023-10-23 16:15:32 +0900

    仮フィードバックUIの構築

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
 
 import Measure from './Measure';
-import { useSpeechRecognition } from 'react-speech-recognition';
 
 export const App = () => {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser dosen't support speach recognition.</span>;
-  }
-
   return (
-    <>
-      <Measure
-        transcript={transcript}
-        listening={listening}
-        resetTranscript={resetTranscript}
-      />
-      
-    </>
+    <div style={{ height: '500px', width: '800px' }}>
+      <Measure />
+    </div>
   );
 };
 
