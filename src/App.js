8c54ce7646d6e1e1698fadfafce3a11b73e0ab5c
8mitsuboy <hachimitsu8897@icloud.com>
2023-10-26 14:42:04 +0900

    250未満、350以上の時の丸の描画位置の変更

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Measure from './Measure';
 
 export const App = () => {
+  const slideTextLengths = [300, 200, 400];
   return (
     <div style={{ height: '500px', width: '800px' }}>
-      <Measure />
+      <Measure slideTextLengths={slideTextLengths} />
     </div>
   );
 };
