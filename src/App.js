8f70e39b2c7571bcaf5ad2845f03e0da6c857268
8mitsuboy <hachimitsu8897@icloud.com>
2023-11-21 16:07:58 +0900

    目標話速度300部分に目標線を引き、エリアの上限値が310未満にならないよう設定した

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Measure from './Measure';
 
 export const App = () => {
-  const slideTextLengths = [200, 300, 400];
+  const slideTextLengths = [175, 425, 425];
   return (
     <div style={{ height: '500px', width: '800px' }}>
       <Measure slideTextLengths={slideTextLengths} />
